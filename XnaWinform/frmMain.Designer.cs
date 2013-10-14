1e6bff983ce9921625940caf3bc9869ab95e9a15
mizrael <davide.guida@razorfishhealthware.com>
2013-10-14 15:00:41 +0200

    first commit

---
+++ XnaWinform/frmMain.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace XnaWinform
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vScrollBar = new System.Windows.Forms.VScrollBar();
+            this.hScrollBar = new System.Windows.Forms.HScrollBar();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // vScrollBar
+            // 
+            this.vScrollBar.Dock = System.Windows.Forms.DockStyle.Right;
+            this.vScrollBar.Location = new System.Drawing.Point(531, 0);
+            this.vScrollBar.Name = "vScrollBar";
+            this.vScrollBar.Size = new System.Drawing.Size(17, 463);
+            this.vScrollBar.TabIndex = 0;
+            this.vScrollBar.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar_Scroll);
+            // 
+            // hScrollBar
+            // 
+            this.hScrollBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.hScrollBar.Location = new System.Drawing.Point(0, 446);
+            this.hScrollBar.Name = "hScrollBar";
+            this.hScrollBar.Size = new System.Drawing.Size(531, 17);
+            this.hScrollBar.TabIndex = 1;
+            this.hScrollBar.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar_Scroll);
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(0, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(531, 446);
+            this.pnlMain.TabIndex = 2;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 463);
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.hScrollBar);
+            this.Controls.Add(this.vScrollBar);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.VScrollBar vScrollBar;
+        private System.Windows.Forms.HScrollBar hScrollBar;
+        private System.Windows.Forms.Panel pnlMain;
+    }
+}
+
---
+++ XnaWinform/frmMain.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using XnaWinform.Extensions;
+using XnaWinform.Services;
+using XnaWinform.UserControls;
+
+namespace XnaWinform
+{
+    public partial class frmMain : Form
+    {
+        private ucRenderer _mapRenderer;
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            this.Load += new EventHandler(Form1_Load);           
+        }
+
+        #region Form Events
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitRenderer();
+        }
+
+        private void vScrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+            var camera = _mapRenderer.Services.GetService<CameraService>();
+            camera.Position.Y += e.NewValue - e.OldValue;
+        }
+
+        private void hScrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+            var camera = _mapRenderer.Services.GetService<CameraService>();
+            camera.Position.X += e.NewValue - e.OldValue;
+        }
+
+        #endregion Form Events
+
+        #region Private Methods
+
+        private void InitRenderer()
+        {
+            _mapRenderer = new ucRenderer();
+            _mapRenderer.Dock = DockStyle.Fill;
+            _mapRenderer.Location = new Point(0, 0);
+            _mapRenderer.TabIndex = 0;
+
+            this.pnlMain.Controls.Add(_mapRenderer);
+        }
+
+        #endregion Private Methods
+
+    }
+}
